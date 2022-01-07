61e18c935a24f6515365b6db4f6d98b96e323026
Monakhrsn <Monakhrsn@gmail.com>
2022-01-07 15:43:56 +0100

    message fo the inlogged user

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { collection, query } from 'firebase/firestore'
 import { useFirestoreQueryData} from '@react-query-firebase/firestore'
 import { db } from '../firebase'
+import { useAuthContext } from '../contexts/AuthContext'
 
 const HomePage = () => {
-	// const ref = collection(db, '')
+	const { currentUser } = useAuthContext()
 	return (
 		<div>
 			<h1>Welcome to my photo app</h1>
+			{currentUser ? <p>You are logged in as {currentUser.email} !</p> : <p>You are not logged in yet</p>}
 		</div>
 	)
 }
