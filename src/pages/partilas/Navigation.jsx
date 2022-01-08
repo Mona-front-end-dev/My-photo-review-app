cde27af2031aa869e3bff6bc263b8e8fcb37eac6
Monakhrsn <Monakhrsn@gmail.com>
2022-01-08 14:49:03 +0100

    refactor navlinks

--- src/pages/partilas/Navigation.jsx
+++ src/pages/partilas/Navigation.jsx
@@ -1,28 +1,39 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink} from 'react-router-dom'
+import Nav from 'react-bootstrap/Nav'
 import Container from 'react-bootstrap/Container'
 import Navbar from 'react-bootstrap/Navbar'
+import NavDropdown from 'react-bootstrap/NavDropdown'
 import 'bootstrap/dist/css/bootstrap.css'
+import { useAuthContext } from '../../contexts/AuthContext'
 
 const Navigation = () => {
-	
+	const { currentUser } = useAuthContext()
   return (
     <Navbar bg="dark" variant="dark" expand="md">
       <Container>
         <Link to="/">
           <span className="router-link">Photo Gallary 🖼️</span>
         </Link>
-        <div display="flex">
-          <Link to="/signup" >
-            <span className="router-link">Sign up</span>
-          </Link>
-          <Link to="/login">
-            <span className="router-link">Login</span>
-          </Link>
-          <Link to="/logout">
-            <span className="router-link">Logout</span>
-          </Link>
-        </div>
+		<Nav className='ms-auto'>
+			{
+				currentUser ? (
+					<>
+						<NavLink to='/' className='nav-link'>Albums</NavLink>
+						<NavDropdown title={currentUser.displayName || currentUser.email} id="basic-nav-dropdown">
+							{/* <NavLink to="/update-profile" className="dropdown-item">Update Profile</NavLink> */}
+							{/* <NavDropdown.Divider /> */}
+							<NavLink to="/logout" className="dropdown-item">Log Out</NavLink>
+						</NavDropdown>
+					</>
+				) : (
+					<>
+						<NavLink to='/signup' className='nav-link'>Signup</NavLink>
+						<NavLink to='/login' className='nav-link'>Login</NavLink>
+					</>
+				)
+			}
+		</Nav>
       </Container>
     </Navbar>
   )
